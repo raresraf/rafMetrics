5e7475ba954fd166b99de754ec9ef05dd0d6ead7
raresraf <rares_folea@yahoo.com>
2019-11-06 23:17:04 +0200

    Fix start bug in website monitor

--- WebMonitoring/WebsiteManager.py
+++ WebMonitoring/WebsiteManager.py
@@ -57,27 +57,26 @@ class WebsiteManager:
                             cursor_insert_request.execute(query)
                             self.cnx.commit()
 
+    def start(self):
+        # Connect to DB
+        self.cnx = mysql.connector.connect(user='root',
+                                           password='password',
+                                           host='10.96.0.2',
+                                           database='WebMonitoring')
 
-def start(self):
-    # Connect to DB
-    self.cnx = mysql.connector.connect(user='root',
-                                       password='password',
-                                       host='10.96.0.2',
-                                       database='WebMonitoring')
+        while True:
+            self.websites = []
 
-    while True:
-        self.websites = []
+            # Check for any update in the list of websites
+            cursor = self.cnx.cursor(buffered=True)
+            query = ('SELECT WebsiteUrl, WebsiteName from WEBSITES')
+            cursor.execute(query)
 
-        # Check for any update in the list of websites
-        cursor = self.cnx.cursor(buffered=True)
-        query = ('SELECT WebsiteUrl, WebsiteName from WEBSITES')
-        cursor.execute(query)
+            for (website_url, website_name) in cursor:
+                self.websites.append((website_url, website_name))
 
-        for (website_url, website_name) in cursor:
-            self.websites.append((website_url, website_name))
-
-        self.run()
-        time.sleep(self.sample_time)
+            self.run()
+            time.sleep(self.sample_time)
 
 
 WebsiteManager(3600).start()
