acb2f20f1023276af08ce115312d5c7b2a2958ce
raresraf <rares_folea@yahoo.com>
2019-11-12 17:07:06 +0200

    Add support for /addwebsite

--- Login/app.py
+++ Login/app.py
@@ -178,6 +178,38 @@ def add_resource():
         cursor.close()
         conn.close()
 
+
+@app.route('/addwebsite', methods=['POST'])
+def add_website():
+    try:
+        _json = request.json
+        _username = _json['username']
+        _website_name = _json['website_name']
+        _url = _json['url']
+        # validate the received values
+        if _username and _website_name and _url and request.method == 'POST':
+            # save edits
+            get_user_info = get_userid(_username)
+            if not get_user_info:
+                return not_found()
+            _userid = get_user_info.get('Userid', 1)
+            sql = "INSERT INTO WEBSITES(Userid, WebsiteName, WebsiteUrl) VALUES(%s, %s, %s)"
+            data = (_userid, _website_name, _url)
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.execute(sql, data)
+            conn.commit()
+            resp = jsonify('Website added successfully!')
+            resp.status_code = 200
+            return resp
+        else:
+            return not_found()
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+
 @app.errorhandler(404)
 def not_found(error=None):
     message = {
