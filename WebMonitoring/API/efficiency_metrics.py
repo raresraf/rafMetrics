f159c0b3267741f4075d5b365c3a5078991fd4a6
Rares Folea <rares_folea@yahoo.com>
2019-11-19 13:41:31 +0200

    Fix efficiency bug div None

--- WebMonitoring/API/efficiency_metrics.py
+++ WebMonitoring/API/efficiency_metrics.py
@@ -2,9 +2,15 @@ import pymysql
 
 
 def div_0(x, y):
-    if y == 0:
+    if not y:
+        return 0
+    if not x:
+        return 0
+    xx = float(x)
+    yy = float(y)
+    if yy == 0:
         return 987654321
-
+    return xx/yy
 
 def get_results_resource_get_efficiency(result_args_get_time,
                                         result_args_old_get_time,
@@ -14,15 +20,14 @@ def get_results_resource_get_efficiency(result_args_get_time,
     result_args_get_efficiency = []
     result_args_old_get_efficiency = []
 
-    for i in range(result_args_get_time):
-        result_args_get_efficiency[i] = div_0(result_args_get_size[i],
-                                              result_args_get_time[i])
-    for i in range(result_args_old_get_time):
-        result_args_old_get_efficiency[i] = div_0(result_args_old_get_size[i],
-                                                  result_args_old_get_time[i])
-
+    for i in range(len(result_args_get_time)):
+        result_args_get_efficiency.append(div_0(result_args_get_size[i],
+                                              result_args_get_time[i]))
+    for i in range(len(result_args_old_get_time)):
+        result_args_old_get_efficiency.append(div_0(result_args_old_get_size[i],
+                                                  result_args_old_get_time[i]))
     list_sample_efficiency = []
-    for i in range(list_sample_time):
+    for i in range(len(list_sample_time)):
         list_sample_efficiency.append(
             div_0(list_sample_size[i], list_sample_time[i]))
 
