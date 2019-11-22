f5f58e6d878126aaf886bbf9a262528882bbdacd
raresraf <rares_folea@yahoo.com>
2019-11-22 17:46:18 +0200

    New deploy procedure

--- deploy_repo.sh
+++ deploy_repo.sh
@@ -9,22 +9,36 @@ if [ -z "$1" ]
     exit
 fi
 
+curr_time=$(date +%Y%m%d%H%M%S)
+
 for var in "$@"
 do
   if [[ $var == "login" ]]
    then
-    ./Login/deploy.sh login
+  sudo docker build -f Dockerfile -t raresraf/login:$curr_time .
+    sudo docker push raresraf/login:$curr_time
+    sed "s/raresraf\/login/raresraf\/login:$curr_time/g" deploy/template_login.yaml > deploy/login.yaml
+    kubectl apply -f deploy/login.yaml
   fi
   if [[ $var == "webmonitoringapi" ]]
-   then
-    ./WebMonitoring/API/deploy.sh webmonitoringapi
+  then
+    sudo docker build -f Dockerfile -t raresraf/webmonitoringapi:$curr_time .
+    sudo docker push raresraf/webmonitoringapi:$curr_time
+    sed "s/raresraf\/webmonitoringapi/raresraf\/webmonitoringapi:$curr_time/g" deploy/template_webmonitoringapi.yaml > deploy/webmonitoringapi.yaml
+    kubectl apply -f deploy/webmonitoringapi.yaml
   fi
   if [[ $var == "resource" ]]
-   then
-    ./WebMonitoring/deploy.sh resource
+  then
+    sudo docker build -f DockerfileResource -t raresraf/resourcemonitor:$curr_time .
+    sudo docker push raresraf/resourcemonitor:$curr_time
+    sed "s/raresraf\/resourcemonitor/raresraf\/resourcemonitor:$curr_time/g" deploy/template_deployment_resource.yaml > deploy/deployment_resource.yaml
+    kubectl apply -f deploy/deployment_resource.yaml
   fi
   if [[ $var == "website" ]]
-   then
-    ./WebMonitoring/deploy.sh website
+  then
+    sudo docker build -f DockerfileWebsite -t raresraf/websitemonitor:$curr_time .
+    sudo docker push raresraf/websitemonitor:$curr_time
+    sed "s/raresraf\/websitemonitor/raresraf\/websitemonitor:$curr_time/g" deploy/template_deployment_website.yaml > deploy/deployment_website.yaml
+	  kubectl apply -f deploy/deployment_website.yaml
   fi
 done
