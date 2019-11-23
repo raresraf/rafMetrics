bbc036eae52b4891d7cce9bde74d58f77b635a3a
raresraf <rares_folea@yahoo.com>
2019-11-23 22:17:25 +0200

    Documentation DB

--- mysql/WebMonitoring/functions/resource_get_availability.sql
+++ mysql/WebMonitoring/functions/resource_get_availability.sql
@@ -6,7 +6,6 @@ CREATE FUNCTION resource_get_availability(RID INT) RETURNS VARCHAR(20)
     BEGIN
         DECLARE COUNT_AVAILABLE INT;
         DECLARE RET_CHAR VARCHAR(20);
-
         select count(*) total into COUNT_AVAILABLE from PING where Resourceid = RID group by Resourceid;
 
         IF COUNT_AVAILABLE IS NULL
