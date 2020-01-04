3276f8eec7afccfde1515b320c2d77162727d287
raresraf <rares_folea@yahoo.com>
2020-01-04 19:33:37 +0200

    .env -> metricsUI/.env

--- metricsUI/deploy.sh
+++ metricsUI/deploy.sh
@@ -13,7 +13,7 @@ if [[ $1 == "metricsui" ]]
    then
 	npm install
 	npm run-script build
-        sudo docker build -f Dockerfile -t raresraf/metricsui:$curr_time .
+  sudo docker build -f Dockerfile -t raresraf/metricsui:$curr_time .
 	sudo docker push raresraf/metricsui:$curr_time
 	sed "s/raresraf\/metricsui/raresraf\/metricsui:$curr_time/g" deploy/template_metricsui.yaml > deploy/metricsui.yaml
 	kubectl apply -f deploy/metricsui.yaml
