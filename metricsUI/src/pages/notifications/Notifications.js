62b997e0a55d7e808afc32e9ed9ea3ec155093f5
raresraf <rares_folea@yahoo.com>
2020-01-04 19:25:03 +0200

    new metricsUI location

--- metricsUI/src/pages/notifications/Notifications.js
+++ metricsUI/src/pages/notifications/Notifications.js
@@ -9,10 +9,10 @@ import classnames from "classnames";
 import "react-toastify/dist/ReactToastify.css";
 import useStyles from "./styles";
 // components
-import Widget from "../../components/Widget/Widget";
-import PageTitle from "../../components/PageTitle/PageTitle";
+import Widget from "../../components/Widget";
+import PageTitle from "../../components/PageTitle";
 import Notification from "../../components/Notification";
-import {Button, Typography} from "../../components/Wrappers/Wrappers";
+import {Button, Typography} from "../../components/Wrappers";
 
 const positions = [
   toast.POSITION.TOP_LEFT,
