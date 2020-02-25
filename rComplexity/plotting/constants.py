2f10f25808fa65a6f259ea21dbc8a11a8dac5996
raresraf <rares_folea@yahoo.com>
2020-02-25 18:48:22 +0200

    plot_points_from_file, test_plot_points_from_file

---
+++ rComplexity/plotting/constants.py
@@ -0,0 +1,4 @@
+from itertools import cycle
+
+plotting_points_symbols = ['o', 'x']
+pool_plotting_points_symbols = cycle(plotting_points_symbols)
---
+++ rComplexity/plotting/plot_points.py
@@ -0,0 +1,29 @@
+import sys
+from itertools import cycle
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from rComplexity.plotting.constants import pool_plotting_points_symbols
+
+
+def plot_points_from_file(filename, matrixCols):
+    # Load Matrix
+    matrix = np.loadtxt(filename, usecols=range(matrixCols + 1))
+
+    for i in range(1, matrixCols + 1):
+        plt.plot(matrix[:, 0],
+                 matrix[:, i],
+                 next(pool_plotting_points_symbols),
+                 label="Sample: " + str(i))
+
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 2:
+        print("Usage: python3 plot_points.py <file_name> <matrixCols>")
+        sys.exit(-1)
+
+    plot_points_from_file(sys.argv[1], int(sys.argv[2]))
