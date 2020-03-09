c915fac9b44b87b171ab3df4668121200bf0997e
raresraf <rares_folea@yahoo.com>
2020-03-09 11:37:07 +0200

    MLengine rafComputing

---
+++ rafComputing/ML/MLdriver.py
@@ -0,0 +1,65 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+# from sklearn.metrics import mean_squared_error, r2_score
+
+from rafComputing.ML.MLengine import LinearRegressionUsingGD
+
+
+def extract_polynomial_features(X, M):
+    phi = np.ones((X.size, M + 1))
+    for i in range(X.size):
+        for j in range(M + 1):
+            phi[i][j] = np.power(X[i], j)
+    return phi
+
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        print("Usage: python3 MLdriver.py <file_name>")
+        sys.exit(-1)
+
+    matrix = np.loadtxt(sys.argv[1], usecols=range(2))
+
+    x = matrix[:, 0]
+    y = matrix[:, 1]
+    x = x.reshape(-1, 1)
+    y = y.reshape(-1, 1)
+
+    orig_x = x
+    x = extract_polynomial_features(x, 3)
+
+    # Model initialization
+    regression_model = LinearRegressionUsingGD()
+
+    # Fit the data(train the model)
+    regression_model.fit(x, y)
+
+    # Predict
+    y_predicted = regression_model.predict(x)
+
+    # model evaluation
+    # rmse = mean_squared_error(y, y_predicted)
+    # r2 = r2_score(y, y_predicted)
+
+    # printing values
+    print('Slope:', regression_model.w_[0][0])
+    # print('Intercept:', regression_model.intercept_)
+    print('Root mean squared error: ', rmse)
+    print('R2 score: ', r2)
+
+    # plotting values
+
+    orig_x = orig_x.flatten()
+    orig_y = y.flatten()
+
+    # data points
+    plt.scatter(orig_x, orig_y, s=10)
+    plt.xlabel('x')
+    plt.ylabel('y')
+
+    y_predicted = y_predicted.flatten()
+    # predicted values
+    plt.plot(orig_x, y_predicted, color='r')
+    plt.show()
---
+++ rafComputing/ML/MLengine.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+
+class LinearRegressionUsingGD:
+    """Linear Regression Using Gradient Descent.
+    Parameters
+    ----------
+    alpha : float
+        Learning rate
+    n_iterations : int
+        No of passes over the training set
+    Attributes
+    ----------
+    w_ : weights/ after fitting the model
+    cost_ : total error of the model after each iteration
+    """
+    def __init__(self, alpha=.1e-20, n_iterations=10000):
+        self.alpha = alpha
+        self.n_iterations = n_iterations
+
+    def fit(self, x, y):
+        """Fit the training data
+        Parameters
+        ----------
+        x : array-like, shape = [n_samples, n_features]
+            Training samples
+        y : array-like, shape = [n_samples, n_target_values]
+            Target values
+        Returns
+        -------
+        self : object
+        """
+
+        self.cost_ = []
+        self.w_ = np.zeros((x.shape[1], 1))
+        m = x.shape[0]
+        for _ in range(self.n_iterations):
+            y_pred = np.dot(x, self.w_)
+            residuals = y_pred - y
+            gradient_vector = np.dot(x.T, residuals)
+            self.w_ -= (self.alpha / m) * gradient_vector
+            cost = np.sum((residuals**2)) / (2 * m)
+            self.cost_.append(cost)
+            print(self.w_)
+        return self
+
+    def predict(self, x):
+        """ Predicts the value after the model has been trained.
+        Parameters
+        ----------
+        x : array-like, shape = [n_samples, n_features]
+            Test samples
+        Returns
+        -------
+        Predicted value
+        """
+        return np.dot(x, self.w_)
