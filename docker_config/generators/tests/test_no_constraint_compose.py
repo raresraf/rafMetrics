4dec262f1be530f0954c8a044e7987c977203619
raresraf <rares_folea@yahoo.com>
2020-04-09 16:06:06 +0300

    repo_docker_compose_no_placement_contraints

---
+++ docker_config/generators/tests/test_no_constraint_compose.py
@@ -0,0 +1,11 @@
+import yaml
+
+from docker_config.generators.no_constraint_compose import remove_placement_contraints
+from docker_config.generators.tests.mocks import SAMPLE_DOCKER_COMPOSE, EXPECTED_DOCKER_COMPOSE
+
+
+def test_remove_placement_contraints():
+    dict = yaml.safe_load(SAMPLE_DOCKER_COMPOSE)
+    dict = remove_placement_contraints(dict)
+    out_dict = yaml.dump(dict, default_flow_style=False, allow_unicode=True)
+    assert out_dict == EXPECTED_DOCKER_COMPOSE
