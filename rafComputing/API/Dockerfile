05927ca0d26f961089fce1815755cb637b5d9b7f
raresraf <rares_folea@yahoo.com>
2020-04-21 21:40:12 +0300

    rafComputing API

---
+++ rafComputing/API/Dockerfile
@@ -0,0 +1,26 @@
+FROM python:3.6-slim
+
+RUN apt-get clean \
+    && apt-get -y update
+
+RUN apt-get -y install \
+    nginx \
+    python3-dev \
+    build-essential
+
+COPY . /rafMetrics
+WORKDIR /rafMetrics
+
+# Install requirements for Python modules
+RUN pip3 install -r requirements.txt
+
+EXPOSE 5000:5000
+
+ENV PYTHONPATH="/rafMetrics"
+ENTRYPOINT ["python3", "./rafComputing/API/app.py"]
+
+# For development purpose only
+# ENTRYPOINT ["tail", "-f", "/dev/null"]
+
+# Local build
+# docker build -f rafComputing/API/Dockerfile -t raresraf/rafcomputingapi:latest .
---
+++ rafComputing/API/tests/mocks.py
@@ -0,0 +1,39 @@
+rafcomputingapi_mock_submit = {
+    "program":
+    "Bubble sort",
+    "name":
+    "results_20200415112451",
+    "data": [
+        {
+            "input": 65536,
+            "output": 17.035170
+        },
+        {
+            "input": 1048576,
+            "output": 4383.926270
+        },
+    ],
+    "sys": {
+        "Architecture": "x86_64",
+        "CPU op-mode(s)": "32-bit, 64-bit",
+        "Byte Order": "Little Endian",
+        "CPU(s)": "4",
+        "On-line CPU(s) list": "0-3",
+        "Thread(s) per core": "1",
+        "Core(s) per socket": "4",
+        "Socket(s)": "1",
+        "NUMA node(s)": "1",
+        "Vendor ID": "GenuineIntel",
+        "CPU family": "6",
+        "Model": "58",
+        "Stepping": "9",
+        "CPU MHz": "3201.000",
+        "BogoMIPS": "6385.77",
+        "Virtualization": "VT-x",
+        "L1d cache": "32K",
+        "L1i cache": "32K",
+        "L2 cache": "256K",
+        "L3 cache": "6144K",
+        "NUMA node0 CPU(s)": "0-3",
+    },
+}
